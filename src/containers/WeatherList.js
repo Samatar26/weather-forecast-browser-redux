f5985ac9a00c785b3710762fcbe40e65c6b66b0a
Samatar Axmed <Samatar_267@hotmail.com>
2017-06-03 20:40:51 +0100

    Created reusable sparlines component AlsaaAlso rendered the temperature and humidity graphs to the app

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from './../components/Chart';
 import uuid from 'uuid/v4';
 
 class WeatherList extends Component {
   renderWeather = cityData => {
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    console.log(cityData.list);
     return (
       <tr key={uuid()}>
         <td>{cityData.city.name}</td>
+        <td><Chart data={temps} color="red" /></td>
+        <td><Chart data={pressure} color="green" /></td>
+        <td><Chart data={humidity} color="blue" /></td>
       </tr>
     );
   };
